02a3c47e00ace94c1ce07cc53c86cf91df20ef78
luana00gomes <luana00gomes@gmail.com>
2025-01-19 02:02:31 -0400

    Creates initial screen

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -6,16 +6,35 @@ import { fontSans } from "@/config/fonts";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Font Awesome */}
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+          rel="stylesheet"
+        />
+        
+        {/* Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=League+Script&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body
         className={clsx(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
+          "body min-h-screen bg-background font-sans antialiased",
+          fontSans.variable // Ensure `fontSans.variable` is defined correctly in `@/config/fonts`.
         )}
       >
         <Main />
         <NextScript />
       </body>
+      <footer className="px-20 py-10 text-center footer">
+        <p>
+          &copy; {new Date().getFullYear()} <a href="https://heroui.com">HeroUI</a>'
+        </p>
+      </footer>
     </Html>
   );
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,60 +1,92 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-import DefaultLayout from "@/layouts/default";
+import { Image } from "@heroui/react";
+import {Card, CardHeader, CardBody, CardFooter} from "@heroui/card";
 
 export default function IndexPage() {
   return (
-    <DefaultLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-xl text-center justify-center">
-          <span className={title()}>Make&nbsp;</span>
-          <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-          <br />
-          <span className={title()}>
-            websites regardless of your design experience.
-          </span>
-          <div className={subtitle({ class: "mt-4" })}>
-            Beautiful, fast and modern React UI library.
-          </div>
-        </div>
-
-        <div className="flex gap-3">
-          <Link
-            isExternal
-            className={buttonStyles({
-              color: "primary",
-              radius: "full",
-              variant: "shadow",
-            })}
-            href={siteConfig.links.docs}
-          >
-            Documentation
-          </Link>
-          <Link
-            isExternal
-            className={buttonStyles({ variant: "bordered", radius: "full" })}
-            href={siteConfig.links.github}
-          >
-            <GithubIcon size={20} />
-            GitHub
-          </Link>
-        </div>
-
-        <div className="mt-8">
-          <Snippet hideCopyButton hideSymbol variant="bordered">
-            <span>
-              Get started by editing{" "}
-              <Code color="primary">pages/index.tsx</Code>
-            </span>
-          </Snippet>
-        </div>
-      </section>
-    </DefaultLayout>
+    <div>
+      <PageOne />
+      <PageTwo />
+    </div>
   );
 }
+
+const PageOne = () => {
+  return (
+    <div className="p-8 col-12 flex flex-row items-center h-screen">
+      <div className="justify w-2/3 mr-20 p-40">
+        <p className="presentation">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+
+        <h1 className="text-8xl	title m-80">Silvana Silva</h1>
+
+        <div className="row flex flex-row items-center">
+          <h5 className="text-xl	subtitle">CRP 000/9889</h5>
+          <i className="fas fa-map-marker-alt ml-6 mr-2" />
+          <h5 className="text-xl subtitle">Rua Tranquedo Neves, Centro</h5>
+
+        </div>
+      </div>
+
+      <div className="content-center">
+        <Image
+          alt="HeroUI hero Image"
+          height={400}
+          radius='full'
+          src="https://heroui.com/images/fruit-1.jpeg"
+          width={400}
+        />
+      </div>
+    </div>
+  )
+}
+
+
+const PageTwo = () => {
+  return (
+    <div className="col-12 flex flex-row py-20 page2">
+      <div className="flex flex-row justify mx-auto gap-20">
+        <CardStyled title='Assunto'>
+          <p className="">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+        </CardStyled>
+
+        <CardStyled title='Assunto'>
+          <p className="">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+        </CardStyled>
+
+        <CardStyled title='Assunto'>
+          <p className="">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+        </CardStyled>
+
+      </div>
+    </div>
+  )
+}
+
+
+interface CardStyledProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const CardStyled = ({ title, children }: CardStyledProps) => {
+  return (
+    <Card className="max-w-[400px] p-5 card">
+      <CardHeader className="flex gap-3">
+        <h1 className="text-lg">{title}</h1>
+      </CardHeader>
+      <CardBody>
+        {children}
+      </CardBody>
+    </Card>
+  );
+}
+
+
